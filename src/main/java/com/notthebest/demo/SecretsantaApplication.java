4b276bcd170709388c9ec19e48d1e14c8ac163c3
vamshibitla <87857985+vamshibitla@users.noreply.github.com>
2025-07-25 08:47:11 +0530

    Update SecretsantaApplication.java

--- src/main/java/com/notthebest/demo/SecretsantaApplication.java
+++ src/main/java/com/notthebest/demo/SecretsantaApplication.java
@@ -1,1 +1,18 @@
-hello
+package com.notthebest.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretsantaApplication extends SpringBootServletInitializer {
+  @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SecretsantaApplication.class);
+    }
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretsantaApplication.class, args);
+	}
+
+}
